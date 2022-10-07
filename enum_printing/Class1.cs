cb6b0d561bac910bf71cdcbd338a8322b2be8e2c
Akshay0087 <104343964+Akshay0087@users.noreply.github.com>
2022-10-07 08:52:15 +0400

    Update Class1.cs

--- enum_printing/Class1.cs
+++ enum_printing/Class1.cs
@@ -10,25 +10,23 @@ namespace Enum_printing
 {
     public class Printing
     {
-
         public static void Print()
         {
             int size = Enum.GetValues(typeof(Range)).Length;
             int previousMax = 0;
             int maxBound;
             int minBound = 0;
+            
             foreach (range alpha in Enum.GetValues(typeof(Range)))
             {
-
                 string tempAlpha = alpha.ToString();
                 maxBound = (int)alpha;
                 minBound = previousMax;
+                
                 Console.WriteLine("Category: " + tempAlpha + " has range of " + minBound + " - " + maxBound);
+                
                 previousMax = maxBound + 1;
-
-
             }
         }
-
     }
 }
