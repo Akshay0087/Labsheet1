51f9cd6533b5b8b0f9329a32390b651646305f77
Akshay0087 <104343964+Akshay0087@users.noreply.github.com>
2022-10-07 08:51:36 +0400

    Update Class1.cs

--- processing2/Class1.cs
+++ processing2/Class1.cs
@@ -9,41 +9,34 @@ namespace Processing2
 {
     public class Processing
     {
-
         public static void ThresholdProcessing()
         {
             int thresholdValue = 0;
 
             Console.Write("Enter number: ");
-
-
             thresholdValue = Int32.Parse(Console.ReadLine());
-
 
             while (thresholdValue != -1)
             {
+                
                 int size = Enum.GetValues(typeof(Range)).Length;
                 int i = 0;
                 int previousMax = 0;
                 int maxBound, minBound;
+                
                 foreach (range alpha in Enum.GetValues(typeof(Range)))
                 {
-
                     string tempAlpha = alpha.ToString();
                     maxBound = (int)alpha;
                     minBound = previousMax;
-                    if (thresholdValue >= minVound && thresholdValue <= maxBound)  
+                    if (thresholdValue >= minBound && thresholdValue <= maxBound)  
                         { 
                         Console.WriteLine("Category " + tempAlpha); 
                         }
-                    previousMax = maxVound + 1;
-
-
-
+                    previousMax = maxBound + 1;
                 }
-
+                
                 Console.Write("Enter new number: ");
-
                 thresholdValue = Int32.Parse(Console.ReadLine());
 
             }
